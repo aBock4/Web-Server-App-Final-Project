f2134fb6541333db8578d243e4c04d5f43efbdfe
arboc <arboc@TABLET-480A185S>
2022-10-27 12:27:08 -0400

    Add project files.

---
+++ Web Server App Final Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Server_App_Final_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
