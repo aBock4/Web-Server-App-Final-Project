d1e80f0ec7eee69a2795e96685496467d1829a43
aBock4 <arbockhorst@gmail.com>
2022-11-03 11:22:11 -0400

    Cars data into HobbyContext

--- Web Server App Final Project/Models/Cars.cs
+++ Web Server App Final Project/Models/Cars.cs
@@ -4,7 +4,7 @@ namespace Web_Server_App_Final_Project.Models
 {
     public class Cars
     {
-        public int Id { get; set; }
+        public int CarId { get; set; }
         public int Year { get; set; }
         public string? Make { get; set; }
         public string? Model { get; set; }
--- Web Server App Final Project/Models/HobbyContext.cs
+++ Web Server App Final Project/Models/HobbyContext.cs
@@ -4,6 +4,33 @@ namespace Web_Server_App_Final_Project.Models
 {
     public class HobbyContext : DbContext
     {
+        public DbSet<Cars>? Cars { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cars>().HasData(
+                new Cars
+                {
+                    CarId = 1,
+                    Year = 1969,
+                    Make = "Ford",
+                    Model = "Mustang Coupe"
+                },
+                new Cars
+                {
+                    CarId = 2,
+                    Year = 2010,
+                    Make = "Ford",
+                    Model = "Focus SE"
+                },
+                new Cars
+                {
+                    CarId = 3,
+                    Year = 1973,
+                    Make = "Jeep",
+                    Model = "J20"
+                }
+            );
+        }
     }
 }
