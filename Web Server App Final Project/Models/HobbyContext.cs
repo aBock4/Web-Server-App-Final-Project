52b8fb525e07adeb1751e43299ac998e61c3a085
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2022-11-03 11:17:10 -0400

    Create DBContext.cs

--- Web Server App Final Project/Models/HobbyContext.cs
+++ Web Server App Final Project/Models/HobbyContext.cs
@@ -4,6 +4,14 @@ namespace Web_Server_App_Final_Project.Models
 {
     public class HobbyContext : DbContext
     {
+        public HobbyContext(DbContextOptions<HobbyContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
 
+        public DbSet<Cars>? Cars { get; set; }
+        public DbSet<Games>? Games { get; set; }
+        public DbSet<Trumpet>? Trumpets { get; set; }
     }
 }
