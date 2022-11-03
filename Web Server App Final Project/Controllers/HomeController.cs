c92cd15656562921f241b7e29a3dc1b93254204e
aBock4 <arbockhorst@gmail.com>
2022-11-03 10:32:23 -0400

    Added NuGet packages; Deleted unnecessary cshtml and corresponding HomeController actions

--- Web Server App Final Project/Controllers/HomeController.cs
+++ Web Server App Final Project/Controllers/HomeController.cs
@@ -17,16 +17,5 @@ namespace Web_Server_App_Final_Project.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
