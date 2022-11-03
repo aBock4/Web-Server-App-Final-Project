3fa0ece86568f256be172ad85f1706136cdb4880
aBock4 <arbockhorst@gmail.com>
2022-11-03 10:16:20 -0400

    Added properties to model.Cars class

--- Web Server App Final Project/Models/Cars.cs
+++ Web Server App Final Project/Models/Cars.cs
@@ -1,7 +1,13 @@
-﻿namespace Web_Server_App_Final_Project.Models
+﻿using Microsoft.VisualBasic;
+
+namespace Web_Server_App_Final_Project.Models
 {
     public class Cars
     {
         public int Id { get; set; }
+        public int Year { get; set; }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        
     }
 }
