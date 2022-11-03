5f8a51d544c692c39b5acd236f9592a00e966300
aBock4 <arbockhorst@gmail.com>
2022-11-03 13:30:39 -0400

    Added HobbiesController with OurData() action

---
+++ Web Server App Final Project/Controllers/HobbiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Server_App_Final_Project.Models;
+
+namespace Web_Server_App_Final_Project.Controllers
+{
+    public class HobbiesController : Controller
+    {
+        private HobbyContext context { get; set; }
+        public HobbiesController(HobbyContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult OurData()
+        {
+            var cars = context.Cars?
+                .OrderBy(m => m.Year)
+                .ToList();
+            return View(cars);
+        }
+    }
+}
