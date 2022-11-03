d9f289a9ae72c0f82a26b64c9287197ba4d7457b
aBock4 <arbockhorst@gmail.com>
2022-11-03 11:11:52 -0400

    Added HobbyContext and builder.Services.AddDbContext

---
+++ Web Server App Final Project/Models/HobbyContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_Server_App_Final_Project.Models
+{
+    public class HobbyContext : DbContext
+    {
+
+    }
+}
--- Web Server App Final Project/Program.cs
+++ Web Server App Final Project/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Web_Server_App_Final_Project.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<HobbyContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HobbyContext")));
 
 var app = builder.Build();
 
