06812901e46e020bcd41e583c0d0dcddfde401bc
aBock4 <arbockhorst@gmail.com>
2022-11-04 10:48:54 -0400

    Added HobbyViewModel to pull from one model for the OurData view

---
+++ Web Server App Final Project/Models/HobbyViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Server_App_Final_Project.Models
+{
+    public class HobbyViewModel
+    {
+        public IEnumerable<Cars>? Cars {get; set;}
+        public IEnumerable<Games>? Games { get; set;}
+        public IEnumerable<Trumpet>? Trumpets { get; set;}
+    }
+}
